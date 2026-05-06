5acfe5c48ce5cb49889a264092a2f9d175d5af32
amina-ameer <63598015+amina-ameer@users.noreply.github.com>
2026-05-06 09:17:21 +0400

    Revert "Merge v13 changes to v15"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/features/home/home.component';
 import { DetailsComponent } from './components/features/details/details.component';
-import { HomeModule } from './components/features/home/home.module';
 
 const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () => import('./components/features/home/home.module').then(m => m.HomeModule)
-  },
-  { path: 'details', component:DetailsComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'details', component:DetailsComponent },
   { path: '**', redirectTo: 'home' }
 ];
 
